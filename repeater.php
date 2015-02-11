7f9f3fb226726f26614941aab720a9e6ee2a7a57
Kelly de Vries <kdv@me.com>
2015-02-10 18:02:47 -0800

    changing file names

--- repeater.php
+++ repeater.php
@@ -1,14 +1,22 @@
 <?php 
-    $repeat_text = $_GET["repeat_text"];
-    $number_of_repeats = $_GET["number_of_repeats"];
-    $repeated_text = str_repeat($repeat_text, $number_of_repeats);
+    $string1 = $_GET["string1"];
+    $string2 = $_GET["string2"];
+    $string3 = $_GET["string3"];
+    $string4 = $_GET["string4"];
+    $string1Fun = strtoupper($string1);
+    $string2Fun = str_word_count($string2);
+    $string3Fun =str_shuffle($string3);
+    $string4Fun =stripos(($string1 . $string2 . $string3 . $string4), cat);
+    $wholeShebang = $string1Fun . $string2Fun . $string3Fun . $string4Fun;
+
 ?>
 <html>
 <head>
-    <title>String Repeating</title>
+    <title>String Games</title>
 </head>
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 <body>
     <h1>Here ya go!</h1>
-    <p><?php echo $repeated_text; ?> </p>
+    <p><?php echo $wholeShebang; ?> </p>
 </body>
 </html>
